d5ccf79b9c07e59352cc3b9157231a96fb62e33f
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2026-08-09 19:02:42 +0300

    feature(hexes): add BoolHexMap conjunction operator

--- tests/Akeldov.Math.Hexes.Tests/Maps/BoolHexMapTests.cs
+++ tests/Akeldov.Math.Hexes.Tests/Maps/BoolHexMapTests.cs
@@ -35,4 +35,83 @@ public class BoolHexMapTests
             Assert.That(values[2], Is.True);
         });
     }
+
+    [Test]
+    public void BitwiseAnd_ReturnsElementWiseConjunctionWithoutChangingOperands()
+    {
+        var topology = new HexMapTopology(2, 2, Layout.EvenQ);
+        var left = new BoolHexMap(topology, new[] { true, true, false, false });
+        var right = new BoolHexMap(topology, new[] { true, false, true, false });
+
+        BoolHexMap result = left & right;
+        Assert.That(result[3], Is.False);
+        result[3] = true;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Topology, Is.EqualTo(topology));
+            Assert.That(result[0], Is.True);
+            Assert.That(result[1], Is.False);
+            Assert.That(result[2], Is.False);
+            Assert.That(result[3], Is.True);
+            Assert.That(left[3], Is.False);
+            Assert.That(right[3], Is.False);
+        });
+    }
+
+    [Test]
+    public void BitwiseAnd_WithEquivalentTopologies_ReturnsElementWiseConjunction()
+    {
+        var left = new BoolHexMap(
+            new HexMapTopology(2, 1, Layout.OddR),
+            new[] { true, false });
+        var right = new BoolHexMap(
+            new HexMapTopology(2, 1, Layout.OddR),
+            new[] { true, true });
+
+        BoolHexMap result = left & right;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result[0], Is.True);
+            Assert.That(result[1], Is.False);
+        });
+    }
+
+    [Test]
+    public void BitwiseAnd_WithDifferentTopologies_Throws()
+    {
+        var left = new BoolHexMap(new HexMapTopology(2, 1, Layout.OddR));
+        var differentResolution = new BoolHexMap(new HexMapTopology(1, 2, Layout.OddR));
+        var differentLayout = new BoolHexMap(new HexMapTopology(2, 1, Layout.EvenR));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(
+                Assert.Throws<ArgumentException>(() => _ = left & differentResolution)!.ParamName,
+                Is.EqualTo("right"));
+            Assert.That(
+                Assert.Throws<ArgumentException>(() => _ = left & differentLayout)!.ParamName,
+                Is.EqualTo("right"));
+        });
+    }
+
+    [Test]
+    public void BitwiseAnd_WithNullOperand_Throws()
+    {
+        var map = new BoolHexMap(new HexMapTopology(1, 1, Layout.OddR));
+        BoolHexMap? missing = null;
+
+        Assert.Multiple(() =>
+        {
+#pragma warning disable CS8604
+            Assert.That(
+                Assert.Throws<ArgumentNullException>(() => _ = missing & map)!.ParamName,
+                Is.EqualTo("left"));
+            Assert.That(
+                Assert.Throws<ArgumentNullException>(() => _ = map & missing)!.ParamName,
+                Is.EqualTo("right"));
+#pragma warning restore CS8604
+        });
+    }
 }
