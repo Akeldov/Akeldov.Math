6074885a4fc60590e8d6bbdadbacdeefb35d6d67
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2026-08-09 16:03:22 +0300

    chore(spatial2d): mark ray intersection API as obsolete

--- src/Akeldov.Math.Spatial2D/Curves/Abstractions/ICurve.cs
+++ src/Akeldov.Math.Spatial2D/Curves/Abstractions/ICurve.cs
@@ -27,6 +27,7 @@ namespace Akeldov.Math.Spatial2D.Curves
         /// <param name="geometryEpsilon">The geometry comparison tolerance in world coordinate units.</param>
         /// <returns>A new mutable list of intersection points in the forward direction of the ray, owned by the caller.</returns>
         /// <exception cref="System.ArgumentOutOfRangeException">Thrown when <paramref name="geometryEpsilon"/> is negative, NaN, or infinite.</exception>
+        [System.Obsolete("This API is obsolete and will be removed in a future version.")]
         List<PointXY> GetRayIntersections(Ray ray, float geometryEpsilon = GeometryConstants.GeometryEpsilon);
 
         /// <summary>
