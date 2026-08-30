f11c7c606d1209ffb5c31c2652b9d574fe6f9f1d
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2026-08-30 22:41:37 +0300

    feature(hexes): sample float fields into spatial hex maps

--- tests/Akeldov.Math.Hexes.Tests/Maps/FloatHexMapExtensionsTests.cs
+++ tests/Akeldov.Math.Hexes.Tests/Maps/FloatHexMapExtensionsTests.cs
@@ -1,4 +1,6 @@
 using Akeldov.Math.Hexes.Geometry;
+using Akeldov.Math.Spatial2D;
+using Akeldov.Math.Spatial2D.Fields;
 
 namespace Akeldov.Math.Hexes.Tests.Maps;
 
@@ -69,6 +71,80 @@ public class FloatHexMapExtensionsTests
     }
 
     [Test]
+    public void ToSpatialHexMap_FromFloatFieldAndGeometry_SamplesHexCenters()
+    {
+        var geometry = new HexMapGeometry(3, 2, new VectorXY(-4f, 8f), 1.5f, Layout.OddR);
+        var centers = new HexCenterMap(geometry);
+        IFloatField field = new CoordinateFloatField();
+
+        SpatialFloatHexMap result = field.ToSpatialHexMap(geometry);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.TypeOf<SpatialFloatHexMap>());
+            Assert.That(result.Geometry, Is.EqualTo(geometry));
+
+            for (int index = 0; index < geometry.Topology.Count; index++)
+                Assert.That(result[index], Is.EqualTo(field.Sample(centers[index])).Within(0.0001f));
+        });
+    }
+
+    [Test]
+    public void ToSpatialHexMap_FromFloatFieldAndHexCenters_SamplesProvidedCenters()
+    {
+        var geometry = new HexMapGeometry(2, 2, new VectorXY(3f, -1f), 0.75f, Layout.EvenQ);
+        var centers = new HexCenterMap(geometry);
+        IFloatField field = new CoordinateFloatField();
+
+        SpatialFloatHexMap result = field.ToSpatialHexMap(centers);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Geometry, Is.EqualTo(geometry));
+
+            for (int index = 0; index < geometry.Topology.Count; index++)
+                Assert.That(result[index], Is.EqualTo(field.Sample(centers[index])).Within(0.0001f));
+        });
+    }
+
+    [Test]
+    public void ToSpatialHexMap_FromFloatField_WhenGeometryIsEmpty_ReturnsEmptySpatialMap()
+    {
+        var geometry = new HexMapGeometry(0, 0, new VectorXY(3f, -2f), 1.25f, Layout.EvenR);
+        IFloatField field = new CoordinateFloatField();
+
+        SpatialFloatHexMap result = field.ToSpatialHexMap(geometry);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Geometry, Is.EqualTo(geometry));
+            Assert.That(result.Topology.Count, Is.Zero);
+        });
+    }
+
+    [Test]
+    public void ToSpatialHexMap_FromFloatField_WhenArgumentsAreInvalid_Throws()
+    {
+        var geometry = new HexMapGeometry(1, 1, 1f, Layout.OddR);
+        IFloatField? field = null;
+        IFloatField source = new CoordinateFloatField();
+        HexCenterMap? hexCenters = null;
+
+#pragma warning disable CS8604
+        var fieldException = Assert.Throws<ArgumentNullException>(() => field.ToSpatialHexMap(geometry));
+        var hexCentersException = Assert.Throws<ArgumentNullException>(() => source.ToSpatialHexMap(hexCenters));
+#pragma warning restore CS8604
+        var geometryException = Assert.Throws<ArgumentOutOfRangeException>(() => source.ToSpatialHexMap(default(HexMapGeometry)));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(fieldException!.ParamName, Is.EqualTo("field"));
+            Assert.That(hexCentersException!.ParamName, Is.EqualTo("hexCenters"));
+            Assert.That(geometryException!.ParamName, Is.EqualTo("geometry"));
+        });
+    }
+
+    [Test]
     public void ToFloatHexMap_ReturnsIndependentMutableCopy()
     {
         var topology = new HexMapTopology(3, 1, Layout.EvenQ);
@@ -133,5 +209,14 @@ public class FloatHexMapExtensionsTests
 #pragma warning restore CS8604
 
         Assert.That(exception!.ParamName, Is.EqualTo("map"));
+    }
+
+    private sealed class CoordinateFloatField : IFloatField
+    {
+        public float Min => -1_000_000f;
+
+        public float Max => 1_000_000f;
+
+        public float Sample(PointXY point) => point.X * 10f + point.Y;
     }
 }
